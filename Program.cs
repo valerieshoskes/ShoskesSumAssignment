45fcd58c9af945f318191c5a8988d2bbef740b9c
valerieshoskes <indi.vs@gmail.com>
2016-09-14 16:03:22 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoskesSumAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] example = { 20, 10, 5, 2 };
+            Console.WriteLine("The sum of the example array is {0}", Sum(example));
+        }
+        static int Sum(int[] myArray)
+        {
+            int answer = 0;
+            foreach (int i in myArray)
+            {
+                answer += i;
+            }
+            return answer;
+        }
+    }
+}
